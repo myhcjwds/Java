21ee0623a5dd53e372717ffbdea66f16deecbe12
myhcjwds <1172105815@qq.com>
2021-02-23 12:49:01 +0800

    第一次commit

--- src/com/company/java/HelloWorld.java
+++ src/com/company/java/HelloWorld.java
@@ -3,6 +3,6 @@ package com.company.java;
 public class HelloWorld {
 
     public static void main(String[] args) {
-	    System.out.println("HelloWord");
+	    System.out.println("HelloWorda");
     }
 }
